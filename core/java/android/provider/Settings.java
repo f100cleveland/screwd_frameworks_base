0a9e5a36778e62287823650a935d0b4c7c929bbf
Muhammed Nazim <mnm9994u@gmail.com>
2013-12-23 18:21:09 +0100

    Option to use volume keys to control media volume anytime [1/2]
    The volume keys control the Media volume rather than the Ringer
    hence removing the issue of accidentaly changing the Ringer volume.
    
    Credits: Pawitp
    Adapted to Marshmallow by zephiK
    
    Change-Id: I789e05bdb0bb7c968f71e4e10f4a759b3dbd0b59
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4215,6 +4215,13 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
+		
+		/**
+		 * Volume key controls ringtone or media sound stream
+         *
+         * @hide
+         */
+        public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
