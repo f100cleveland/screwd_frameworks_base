f146f5346ffa2111eb804e9129bf031dbe7cec19
Shuhao Wu <shuhao@shuhaowu.com>
2015-06-22 20:21:14 -0400

    [1/2] Allow toggling animations off
    Allow for toggling the animations that comes by default with Android L,
    such as the open app animation, or other transition animations. This
    effect is the same as when it goes into lower power mode, except nothing
    else is affected.
    
    A change in packages/app/Settings is also required.
    
    Change-Id: I9b19e043386898a75f1ff02941ecdddbf6116711
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4007,6 +4007,12 @@ public final class Settings {
         public static final String STATUS_BAR_IME_ARROWS = "status_bar_ime_arrows";
 
         /**
+         * Disabled Transition animations of the android system
+         * @hide
+         */
+        public static final String DISABLE_TRANSITION_ANIMATIONS = "disable_transition_animations";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
