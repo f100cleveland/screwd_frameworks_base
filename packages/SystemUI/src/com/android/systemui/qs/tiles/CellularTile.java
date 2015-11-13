9ba1ad7c45390d3547dca59dd494d348e52d3cfb
beanstown106 <nbenis106@gmail.com>
2015-11-13 09:41:30 -0500

    Longpress actions for all quicksettings buttons *also break the is recently used dependency on hotspot and inversion *airplane mode > wireless settings *bluetooth > bluetooth settings *cast > cast settings *cellular tile > mobile network settings *color inversion > accesibility settings *do not disturb > do not disturb settings *hotspot tile > hotspot settings *location tile > location settings *wifi tile > wifi settings
    Change-Id: I326b9b8b04c05c384588d75bf7ac5c812022cc32
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
    	packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
    	packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -89,6 +89,11 @@ public class CellularTile extends QSTile<QSTile.SignalState> {
     }
 
     @Override
+    protected void handleLongClick() {
+        mHost.startActivityDismissingKeyguard(MOBILE_NETWORK_SETTINGS);
+    }
+
+    @Override
     protected void handleUpdateState(SignalState state, Object arg) {
         state.visible = mController.hasMobileDataFeature();
         if (!state.visible) return;
