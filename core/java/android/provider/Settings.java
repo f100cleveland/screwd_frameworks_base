6d1a88b90329f808595eed652597611aa480fa9f
maxwen <max.weninger@gmail.com>
2015-12-03 05:33:05 -0500

    [1/2] base: unlink notification volume (Squashed) because of omni merging 6.0 into 5.1 just cherrypicking their commit wasnt gonna work, and had undesired consequences this squashs all the relevant parts needed for 6.0
    Squashed By: @BeansTown106
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/android/preference/SeekBarVolumizer.java
    	core/java/android/provider/Settings.java
    	core/res/res/values/purenexus_symbols.xml
    	services/core/java/com/android/server/audio/AudioService.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4279,6 +4279,7 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
         /**
+<<<<<<< HEAD
          * Current active provider
          * @hide
          */
@@ -4305,6 +4306,12 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_FULL_SCREEN = "recents_full_screen";
+		
+		/**
+		 * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
