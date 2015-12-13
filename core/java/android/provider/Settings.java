a0133eb7ed42e8354493df7a71aab1ded71ccb7f
LorDClockaN <davor@losinj.com>
2015-12-13 22:11:08 +0100

    FWB: PIE: Always on the right switch (1/2)
    Change-Id: I1b5937af6d36d8272261ca1668f290c5b51ee395
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4530,6 +4530,11 @@ public final class Settings {
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
         /**
+         * @hide
+         */
+        public static final String PA_PIE_ALWAYS_RIGHT = "pa_pie_always_right";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
