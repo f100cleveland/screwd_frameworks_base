CyanogenMod/android_frameworks_base

Live lock screen support [1/2]
Change-Id: I15bc712b2a143f72adbfa61fad94fcfe18b98379

core/java/android/content/pm/ThemeUtils.java
@@ -630,6 +630,7 @@ public class ThemeUtils {
         components.add(ThemesColumns.MODIFIES_RINGTONES);
         components.add(ThemesColumns.MODIFIES_STATUS_BAR);
         components.add(ThemesColumns.MODIFIES_NAVIGATION_BAR);
+        components.add(ThemesColumns.MODIFIES_ANIMATED_LOCKSCREEN);
         return components;
     }
 

core/java/android/content/res/ThemeChangeRequest.java
@@ -79,6 +79,10 @@ public final class ThemeChangeRequest implements Parcelable {
         return getThemePackageNameForComponent(MODIFIES_RINGTONES);
     }
 
+    public String getAnimatedLockScreenThemePackageName() {
+        return getThemePackageNameForComponent(MODIFIES_ANIMATED_LOCKSCREEN);
+    }
+
     public final Map<String, String> getThemeComponentsMap() {
         return Collections.unmodifiableMap(mThemeComponents);
     }
@@ -237,6 +241,10 @@ public final class ThemeChangeRequest implements Parcelable {
 
         public Builder setRingtone(String pkgName) {
             return setComponent(MODIFIES_RINGTONES, pkgName);
+        }
+
+        public Builder setAnimatedLockScreen(String pkgName) {
+            return setComponent(MODIFIES_ANIMATED_LOCKSCREEN, pkgName);
         }
 
         public Builder setComponent(String component, String pkgName) {

f100cleveland/screwd_frameworks_base

Change-Id: Iafb09bb77e10c89fb4b76cc807ca5bf86c8ba97b
Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

core/java/android/content/pm/ThemeUtils.java
@@ -630,6 +630,7 @@ public class ThemeUtils {
         components.add(ThemesColumns.MODIFIES_RINGTONES);
         components.add(ThemesColumns.MODIFIES_STATUS_BAR);
         components.add(ThemesColumns.MODIFIES_NAVIGATION_BAR);
+        components.add(ThemesColumns.MODIFIES_LIVE_LOCK_SCREEN);
         return components;
     }
 

core/java/android/content/res/ThemeChangeRequest.java
@@ -79,6 +79,10 @@ public final class ThemeChangeRequest implements Parcelable {
         return getThemePackageNameForComponent(MODIFIES_RINGTONES);
     }
 
+    public String getLiveLockScreenThemePackageName() {
+        return getThemePackageNameForComponent(MODIFIES_LIVE_LOCK_SCREEN);
+    }
+
     public final Map<String, String> getThemeComponentsMap() {
         return Collections.unmodifiableMap(mThemeComponents);
     }
@@ -237,6 +241,10 @@ public final class ThemeChangeRequest implements Parcelable {
 
         public Builder setRingtone(String pkgName) {
             return setComponent(MODIFIES_RINGTONES, pkgName);
+        }
+
+        public Builder setLiveLockScreen(String pkgName) {
+            return setComponent(MODIFIES_LIVE_LOCK_SCREEN, pkgName);
         }
 
         public Builder setComponent(String component, String pkgName) {