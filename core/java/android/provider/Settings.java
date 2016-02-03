b75795b07746498b9d9613f8221373332325b69b
rogersb11 <brettrogers11@gmail.com>
2016-02-02 22:46:10 -0500

    Heads Up Timeout && Snooze  2/2
    Change-Id: I776a9b5b2f411e4940070e76ce705fc7f055b689

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4850,6 +4850,20 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Timeout for Heads Up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
+         * Snooze time for heads up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
