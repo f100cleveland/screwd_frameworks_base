249eed0ee19b1f74c07bbef2ec28eb47e44ac999
Corey Edwards <ensabahnur16@gmail.com>
2016-02-22 09:35:31 -0600

    Update screwdUtils
    Needs the deviceSupportsFlashlight method
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/com/android/internal/util/screwd/screwdUtils.java
+++ core/java/com/android/internal/util/screwd/screwdUtils.java
@@ -19,6 +19,9 @@ package com.android.internal.util.screwd;
 import android.content.Context;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 
 import java.util.Locale;
 
@@ -38,6 +41,28 @@ public class screwdUtils {
         ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
+    }
+	
+	public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
     }
 
 }
